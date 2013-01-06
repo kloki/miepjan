42713cffc666ea2d1234b2ea309e2dc98a17ded1
kloki <k.klinkers@gmail.com>
2013-01-06 13:37:30 +0100

    wrong image

--- main.lua
+++ main.lua
@@ -1,20 +1,18 @@
-# kloki
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Koen Klinkers k.klinkers@gmail.com
+-- kloki
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.-
 
+-- Koen Klinkers k.klinkers@gmail.com
 
 
 function love.load()
@@ -22,10 +20,14 @@ function love.load()
    screenheight=640
    screenwidth=1024
    debug=false
+   mode=0
+   background=love.graphics.newImage("images/background.jpg")
+   fSmall=love.graphics.newFont("font/ChronoTrigger.ttf",20)
+   fBig=love.graphics.newFont("font/ChronoTrigger.ttf",50)
 end
 
 function love.draw()
-
+   love.graphics.draw(background,0,0)
 end
 
 function love.update(dt)
